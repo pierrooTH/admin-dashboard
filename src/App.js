d9b27e685121f23f310962643b8661004312e9f2
pierrooTH <pierre.thouvenot1711@gmail.com>
2021-05-20 21:57:33 +0200

    Initialization TP admin dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    todos: []
+  }
+  getTodo = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        todos: res.data
+      })
+    })
+  }
+  render() {
+    const listTodos = this.state.todos.map(todo => {
+      return <li>{todo.title}</li>
+    })
+    return (
+      <div className="app">
+        <button onClick={this.getTodo}>Clique</button>
+
+        <ul>
+          {listTodos}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
